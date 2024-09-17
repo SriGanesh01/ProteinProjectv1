f1fdf5dd8ec8c0e0a8e4d226a83cc48a9c330478
SriGanesh01 <psriganesh2005@gmail.com>
2024-09-17 17:43:25 +0530

    Added Functional changes

--- ProteinProjectv1/main.cpp
+++ ProteinProjectv1/main.cpp
@@ -5,74 +5,93 @@
 #include <stdio.h>
 #include <GL/glew.h>
 #include <GLFW/glfw3.h>
+
+using namespace ImGui;
+
 // Window dimensions
 const GLint WIDTH = 800, HEIGHT = 600;
-int APIENTRY WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nCmdShow)
 
+void makew()
 {
-	// Initialize GLFW
-	if (!glfwInit())
+    ImGui_ImplOpenGL3_NewFrame();
+    ImGui_ImplGlfw_NewFrame();
+    ImGui::NewFrame();
+
+	if (ImGui::Begin("Test Window 1"))
 	{
-		printf("GLFW initialization failed\n");
-		return 1;
-	}
-	// Setup GLFW window properties
-	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
-	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
-	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE); // No Backwards Compatibility
-		glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE); // Forward Compatibility
-		GLFWwindow* mainWindow = glfwCreateWindow(WIDTH, HEIGHT, "Test Window",
-			NULL, NULL);
-	if (!mainWindow)
-	{
-		printf("GLFW window creation failed\n");
-		glfwTerminate();
-		return 1;
-	}
-	// Get buffer size information
-	GLint bufferWidth, bufferHeight;
-	glfwGetFramebufferSize(mainWindow, &bufferWidth, &bufferHeight);
-	// Set context for GLEW to use
-	glfwMakeContextCurrent(mainWindow);
-	// Allow modern extension features
-	glewExperimental = GL_TRUE;
-	if (glewInit() != GLEW_OK)
-	{
-		printf("GLEW initialization failed\n");
-		glfwDestroyWindow(mainWindow);
-		glfwTerminate();
-		return 1;
-	}
-	// Setup Viewport size
-	glViewport(0, 0, bufferWidth, bufferHeight);
-	IMGUI_CHECKVERSION();
-	ImGui::CreateContext();
-	ImGuiIO& io = ImGui::GetIO(); (void)io;
-	ImGui::StyleColorsDark();
-	ImGui_ImplGlfw_InitForOpenGL(mainWindow, true);
-	ImGui_ImplOpenGL3_Init("#version 330");
-	// Loop until window closed
-	while (!glfwWindowShouldClose(mainWindow))
-	{
-		glfwPollEvents();
-		// Clear window
-		glClearColor(1.0f, 0.2f, 0.2f, 1.0f);
-		glClear(GL_COLOR_BUFFER_BIT);
-		ImGui_ImplOpenGL3_NewFrame();
-		ImGui_ImplGlfw_NewFrame();
-		ImGui::NewFrame();
-		ImGui::Begin("Test Window");
 		ImGui::Text("Hello World");
-		ImGui::End();
-		ImGui::Render();
-		ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
-		glfwSwapBuffers(mainWindow);
-	}
-	ImGui_ImplOpenGL3_Shutdown();
-	ImGui_ImplGlfw_Shutdown();
-	ImGui::DestroyContext();
-	// Clean up and exit
-	glfwDestroyWindow(mainWindow);
-	glfwTerminate();
-	return 0;
+	} ImGui::End();
+
+	ImGui::SetNextWindowSize(ImVec2(200, 100), ImGuiCond_FirstUseEver);
+    if (ImGui::Begin("Test Window 2", NULL, ImGuiWindowFlags_NoCollapse | ImGuiWindowFlags_NoResize))
+    {
+        ImGui::Text("Hello World");
+    } ImGui::End();
+
+    ImGui::Render();
+    ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
+}
+
+int APIENTRY WinMain(_In_ HINSTANCE hInstance, _In_opt_ HINSTANCE hPrevInstance, _In_ LPSTR lpCmdLine, _In_ int nCmdShow)
+{
+    // Initialize GLFW
+    if (!glfwInit())
+    {
+        printf("GLFW initialization failed\n");
+        return 1;
+    }
+    // Setup GLFW window properties
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE); // No Backwards Compatibility
+    glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE); // Forward Compatibility
+    GLFWwindow* mainWindow = glfwCreateWindow(WIDTH, HEIGHT, "Test Window", NULL, NULL);
+    if (!mainWindow)
+    {
+        printf("GLFW window creation failed\n");
+        glfwTerminate();
+        return 1;
+    }
+    // Get buffer size information
+    GLint bufferWidth, bufferHeight;
+    glfwGetFramebufferSize(mainWindow, &bufferWidth, &bufferHeight);
+    // Set context for GLEW to use
+    glfwMakeContextCurrent(mainWindow);
+    // Allow modern extension features
+    glewExperimental = GL_TRUE;
+    if (glewInit() != GLEW_OK)
+    {
+        printf("GLEW initialization failed\n");
+        glfwDestroyWindow(mainWindow);
+        glfwTerminate();
+        return 1;
+    }
+    // Setup Viewport size
+    glViewport(0, 0, bufferWidth, bufferHeight);
+
+    IMGUI_CHECKVERSION();
+    ImGui::CreateContext();
+    ImGuiIO& io = ImGui::GetIO(); (void)io;
+    ImGui::StyleColorsDark();
+    ImGui_ImplGlfw_InitForOpenGL(mainWindow, true);
+    ImGui_ImplOpenGL3_Init("#version 330");
+    // Loop until window closed
+    while (!glfwWindowShouldClose(mainWindow))
+    {
+        glfwPollEvents();
+        // Clear window
+        glClearColor(1.0f, 0.2f, 0.2f, 1.0f);
+        glClear(GL_COLOR_BUFFER_BIT);
+		makew();
+        glfwSwapBuffers(mainWindow);
+    }
+
+    ImGui_ImplOpenGL3_Shutdown();
+    ImGui_ImplGlfw_Shutdown();
+    ImGui::DestroyContext();
+
+    // Clean up and exit
+    glfwDestroyWindow(mainWindow);
+    glfwTerminate();
+    return 0;
 }
