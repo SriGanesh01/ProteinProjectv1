3fb73c56a784c586b8cd9fa374e4e08a7322661b
SriGanesh01 <psriganesh2005@gmail.com>
2024-12-10 01:02:20 +0530

    Changes To Color

--- ProteinProjectv1/main.cpp
+++ ProteinProjectv1/main.cpp
@@ -55,7 +55,7 @@ int APIENTRY WinMain(_In_ HINSTANCE hInstance, _In_opt_ HINSTANCE hPrevInstance,
     {
         glfwPollEvents();
         // Clear window
-        glClearColor(1.0f, 0.2f, 0.2f, 1.0f);
+        glClearColor(0.2f, 0.2f, 0.2f, 0.2f);
         glClear(GL_COLOR_BUFFER_BIT);
 
 		//TODO call the loop of the LineRenderer
