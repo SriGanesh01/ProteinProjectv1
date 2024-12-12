97804f48bc6306083cf80651710f7bddca1bca4d
AmruthaGetta <amruthagetta13@gmail.com>
2024-12-12 22:11:44 +0530

    linerenderer added

--- ProteinProjectv1/LineRenderer.cpp
+++ ProteinProjectv1/LineRenderer.cpp
@@ -1,9 +1,4 @@
 #include "AllHeader.h"
-#include "main_ui.h"
-
-using namespace ImGui;
-
-const GLint WIDTH = 800, HEIGHT = 600;
 
 void render() {
     static GLuint VAO = 0, VBO = 0;
@@ -24,61 +19,4 @@ void render() {
     glBindVertexArray(VAO);
     glDrawArrays(GL_LINES, 0, 2);
     glBindVertexArray(0);
-}
-
-int APIENTRY WinMain(_In_ HINSTANCE hInstance, _In_opt_ HINSTANCE hPrevInstance, _In_ LPSTR lpCmdLine, _In_ int nCmdShow) {
-    if (!glfwInit()) {
-        printf("GLFW initialization failed\n");
-        return 1;
-    }
-
-    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
-    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
-    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
-    glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
-
-    GLFWwindow* mainWindow = glfwCreateWindow(WIDTH, HEIGHT, "Test Window", NULL, NULL);
-    if (!mainWindow) {
-        printf("GLFW window creation failed\n");
-        glfwTerminate();
-        return 1;
-    }
-
-    glfwMakeContextCurrent(mainWindow);
-
-    glewExperimental = GL_TRUE;
-    if (glewInit() != GLEW_OK) {
-        printf("GLEW initialization failed\n");
-        glfwDestroyWindow(mainWindow);
-        glfwTerminate();
-        return 1;
-    }
-
-    GLint bufferWidth, bufferHeight;
-    glfwGetFramebufferSize(mainWindow, &bufferWidth, &bufferHeight);
-    glViewport(0, 0, bufferWidth, bufferHeight);
-
-    IMGUI_CHECKVERSION();
-    ImGui::CreateContext();
-    ImGui_ImplGlfw_InitForOpenGL(mainWindow, true);
-    ImGui_ImplOpenGL3_Init("#version 330");
-
-    while (!glfwWindowShouldClose(mainWindow)) {
-        glfwPollEvents();
-        glClearColor(1.0f, 0.75f, 0.8f, 1.0f);
-
-        glClear(GL_COLOR_BUFFER_BIT);
-
-        render();
-        MainGUI1();
-        glfwSwapBuffers(mainWindow);
-    }
-
-    ImGui_ImplOpenGL3_Shutdown();
-    ImGui_ImplGlfw_Shutdown();
-    ImGui::DestroyContext();
-    glfwDestroyWindow(mainWindow);
-    glfwTerminate();
-
-    return 0;
 }
--- ProteinProjectv1/LineRenderer.h
+++ ProteinProjectv1/LineRenderer.h
@@ -1,10 +1,2 @@
-#ifndef LINERENDER_H
-#define LINERENDER_H
-
-#include <GL/glew.h>
-extern const GLint WIDTH;
-extern const GLint HEIGHT;
-
+#pragma once
 void render();
-
-#endif 
